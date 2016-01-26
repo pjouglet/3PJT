6e2666c93e0c1c89d6a0c04b3fe6fc06e435b0b0
Pierre JOUGLET <171296@supinfo.com>
2016-01-26 09:08:05 +0100

    Ajout socle Symfony

---
+++ website/README.md
@@ -0,0 +1,4 @@
+project
+=======
+
+A Symfony project created on January 26, 2016, 8:57 am.
---
+++ website/src/Supinfo/CommanderBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Supinfo\CommanderBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('SupinfoCommanderBundle:Default:index.html.twig');
+    }
+}
