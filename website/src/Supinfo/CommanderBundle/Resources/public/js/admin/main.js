d91172326e2780e9e7059d6b03917c589b02f476
Pierre JOUGLET <171296@supinfo.com>
2016-05-18 22:43:22 +0200

    Pages sur les clients (vue globale du client, liste des clients)

--- website/src/Supinfo/CommanderBundle/Resources/public/js/admin/main.js
+++ website/src/Supinfo/CommanderBundle/Resources/public/js/admin/main.js
@@ -10,17 +10,52 @@ $(document).ready(function(){
     $(".delete_employee").click(function(){
         var url = $(this).children('span').text();
         swal({
-            title: "Attention",
-            text: "Cette action sera définitive. L'employé sera supprimé, continuer ?",
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            closeOnConfirm: false
-        },
-        function (isConfirm) {
-           if(isConfirm)
-               window.location.href = url;
-        });
+                title: "Attention",
+                text: "Cette action sera définitive. L'employé sera supprimé, continuer ?",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                closeOnConfirm: false
+            },
+            function (isConfirm) {
+                if(isConfirm)
+                    window.location.href = url;
+            });
         return false;
-    })
+    });
+
+    //page customers
+    $(".delete_customer").click(function(){
+        var url = $(this).children('span').text();
+        swal({
+                title: "Attention",
+                text: "L'utilisateur ne sera pas supprimé, son compte sera juste désactiver. Continuer ?",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                closeOnConfirm: false
+            },
+            function (isConfirm) {
+                if(isConfirm)
+                    window.location.href = url;
+            });
+        return false;
+    });
+
+    $(".activate_customer").click(function(){
+        var url = $(this).children('span').text();
+        swal({
+                title: "Attention",
+                text: "Réactiver le compte utilisateur permettra à celui-ci de se connecter à nouveau. Continuer ?",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                closeOnConfirm: false
+            },
+            function (isConfirm) {
+                if(isConfirm)
+                    window.location.href = url;
+            });
+        return false;
+    });
 });
